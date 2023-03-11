348f8142227bbb2b402e53a87c3d10a58c550b55
Vadim <timecatcher08@gmail.com>
2023-03-11 20:09:41 +0300

    vite config changed for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import * as path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-landing/",
   plugins: [react()],
-  resolve: {    alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }]  },
-  test:{
-    environment: "jsdom"
-  }
-})
+  resolve: {
+    alias: [{ find: "~", replacement: path.resolve(__dirname, "src") }],
+  },
+  test: {
+    environment: "jsdom",
+  },
+});
